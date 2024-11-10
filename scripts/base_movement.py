a047481b4e33514e77674822444c0b493366a852
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2024-11-10 23:17:53 +0100

    Add files via upload

--- scripts/base_movement.py
+++ scripts/base_movement.py
@@ -4,10 +4,11 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
 
+
 def straight(linear_speed, distance):
     # initialize the node
     rospy.init_node('basic_motion_node', anonymous=True)
-    
+
     # publisher
     pub = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)  # 10 Hz
@@ -17,16 +18,24 @@ def straight(linear_speed, distance):
     move_cmd.linear.x = linear_speed
     move_cmd.angular.z = 0
 
-    rospy.loginfo("Robot is going straight... Press Ctrl+C to stop.")
-    start_time = rospy.Time.now()
-    while not rospy.is_shutdown() or (rospy.Time.now() - start_time).to_sec() >= (distance/linear_speed):
-        pub.publish(move_cmd)
+    # setup the clock
+    while rospy.Time.now().to_sec() == 0:
+        rospy.loginfo("Waiting for ROS time to initialize...")
         rate.sleep()
-        
+    start_time = rospy.Time.now()
+
+    # publish the cmd_vel
+    rospy.loginfo("Robot is going straight... Press Ctrl+C to stop.")
+    while not (rospy.is_shutdown() or (rospy.Time.now() - start_time).to_sec() > (distance / abs(linear_speed))):
+        pub.publish(move_cmd)
+        print('running', (rospy.Time.now() - start_time).to_sec(), 's')
+        rate.sleep()
+
+
 def rotate(angular_speed, angle):
     # initialize the node
     rospy.init_node('basic_motion_node', anonymous=True)
-    
+
     # publisher
     pub = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)  # 10 Hz
@@ -35,17 +44,25 @@ def rotate(angular_speed, angle):
     move_cmd = Twist()
     move_cmd.linear.x = 0
     move_cmd.angular.z = angular_speed
-    
-    rospy.loginfo("Robot is rotating... Press Ctrl+C to stop.")
-    start_time = rospy.Time.now()
-    while not rospy.is_shutdown() or (rospy.Time.now() - start_time).to_sec() >= (angle/angular_speed):
-        pub.publish(move_cmd)
+
+    # setup the clock
+    while rospy.Time.now().to_sec() == 0:
+        rospy.loginfo("Waiting for ROS time to initialize...")
         rate.sleep()
-        
+    start_time = rospy.Time.now()
+
+    # publish the cmd_vel
+    rospy.loginfo("Robot is rotating... Press Ctrl+C to stop.")
+    while not (rospy.is_shutdown() or (rospy.Time.now() - start_time).to_sec() > (angle / abs(angular_speed))):
+        pub.publish(move_cmd)
+        print('running', (rospy.Time.now() - start_time).to_sec(), 's')
+        rate.sleep()
+
+
 def move_in_circle(linear_speed, radius):
     # initialize the node
     rospy.init_node('basic_motion_node', anonymous=True)
-    
+
     # publisher
     pub = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)  # 10 Hz
@@ -63,18 +80,20 @@ def move_in_circle(linear_speed, radius):
         pub.publish(move_cmd)
         rate.sleep()
 
+
 def switch_mode(case):
     if case == 1:
-        straight(linear_speed=0.1)
+        straight(linear_speed=-0.1, distance=0.5)
     elif case == 2:
-        rotate(angular_speed=np.pi/5)
+        rotate(angular_speed=np.pi / 6, angle=np.pi / 2)
     elif case == 3:
         move_in_circle(linear_speed=0.1, radius=1)
     else:
         print('not a valid movement mode!')
-        
+
+
 if __name__ == '__main__':
     try:
-        switch_mode(3)
+        switch_mode(1)
     except rospy.ROSInterruptException:
         pass
